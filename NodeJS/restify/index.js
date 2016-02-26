e5ac6000f43534e927254f6391e20ddbbf1668e2
Rodrigo Navarro <rnavarro1@gmail.com>
2016-02-25 22:10:01 -0300

    Adding Express and Restify.

---
+++ NodeJS/restify/index.js
@@ -0,0 +1,23 @@
+var cluster = require("cluster");
+
+if (cluster.isMaster) {
+  var cpus = require("os").cpus().length;
+  for (var i = 0; i < cpus; i++) cluster.fork();
+} else {
+  function fibonacci(n) {
+    switch (n) {
+      case 0: return 0
+      case 1: return 1
+      default: return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+  }
+
+  var restify = require("restify");
+  var app = restify.createServer();
+
+  app.get("/:number", function(req, res) {
+    res.end(fibonacci(req.params.number).toString());
+  });
+
+  app.listen(4000);
+}
