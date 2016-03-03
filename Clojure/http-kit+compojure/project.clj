896b6824b46e80e2e0294535d626d69b13d306c1
Wagner Amaral <wamaral@bionexo.com>
2016-03-03 18:04:47 -0300

    Adding clojure files

---
+++ Clojure/http-kit+compojure/project.clj
@@ -0,0 +1,11 @@
+(defproject fib "0.1.0-SNAPSHOT"
+  :description "Fibonacci in Clojure + http-kit + compojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.4.0"]
+                 [javax.servlet/servlet-api "2.5"]
+                 [http-kit "2.1.19"]]
+  :main ^:skip-aot fib.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
