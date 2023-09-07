d1259d6bd12fb62c9d5ddd8b345ac60afb40c11c
LionyxML <rahul.juliato@gmail.com>
2023-09-07 12:00:47 -0300

    feat: elisp implementation with simple-httpd

---
+++ Elisp/simple-httpd/server.el
@@ -0,0 +1,21 @@
+(add-to-list 'load-path "./packages")
+(require 'simple-httpd)
+
+(setq httpd-port 4000)
+
+(defun fib (n)
+  (if (< n 2)
+      n
+    (+ (fib (- n 1)) (fib (- n 2)))))
+
+
+(defservlet* :number text/plain (verbose)
+   (insert (format "%d\n" (fib (string-to-number number)))))
+
+
+(httpd-start)
+
+(message "Server running on port %d" httpd-port)
+
+(while t
+  (sleep-for 5))
